eb411839ab86c291bf6b8b8baf7b20e301386076
Inamullah khan <inamullah7081@gmail.com>
2025-07-19 11:53:02 -0700

    Transfer Google form into Android app

--- app/src/main/java/com/example/webconverttoapp/MainActivity.kt
+++ app/src/main/java/com/example/webconverttoapp/MainActivity.kt
@@ -1,14 +1,37 @@
-import com.example.webconverttoapp.R
+package com.example.webconverttoapp
+import android.os.Build
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.webconverttoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var m_binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val webView: WebView = findViewById(R.id.WebToApp)
-        webView.loadUrl("https://yourwebsite.com")
+        m_binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(m_binding.root)
+
+       val webview: WebView=m_binding.webToApp
+        webViewApp(webview)
+
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewApp(a: WebView){
+        a.webViewClient= WebViewClient()
+
+        a.apply {
+            settings.javaScriptEnabled=true
+            settings.safeBrowsingEnabled=true
+            loadUrl("https://docs.google.com/forms/d/e/1FAIpQLScHlpb7Awou6jIgmM5euNNeXrhwXpiar8UGuOsU1AD3Ng8jeg/viewform?usp=dialog")
+
+        }
+
     }
 }
 
